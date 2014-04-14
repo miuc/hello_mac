423c4e803d68fe7863166b67b928d8bf5f1ef015
同强 陈 <tongqiangchen@tongqiangdeMacBook-Pro.local>
2014-04-15 00:15:23 +0800

    Initial Commit

---
+++ hello_world/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello_world
+//
+//  Created by 同强 陈 on 14-4-15.
+//  Copyright (c) 2014年 同强 陈. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
