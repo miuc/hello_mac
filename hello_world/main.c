8bdeb301434832420f12d4faad6f47958a09f126
miuc <miuc.cpp@gmail.com>
2014-04-27 23:56:00 +0800

    add mem test

--- hello_world/main.c
+++ hello_world/main.c
@@ -2,17 +2,43 @@
 //  main.c
 //  hello_world
 //
-//  Created by 同强 陈 on 14-4-15.
-//  Copyright (c) 2014年 同强 陈. All rights reserved.
+//  Created by Tongqiang Chen on 14-4-15.
+//  Copyright (c) 2014年 Tongqiang Chen. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void my_print()
+{
+    int i;
+    int *p = &i;
+    
+    void * heap = malloc(sizeof(i));
+    
+    printf("int length:%ld\n", sizeof(i));
+    printf("point length:%ld\n", sizeof(p));
+    
+    printf("stack position 1:%p\n", p);
+    printf("stack position 2:%p\n", &p);
+    
+    if (heap == NULL)
+    {
+        printf("malloc heap data fail..\n");
+        exit(-1);
+    }
+    
+    printf("heap position 1:%p\n", heap);
+    
+    void * heap2 = malloc(sizeof(i));
+    printf("heap position 2:%p\n", heap2);
+}
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     printf("Hello, World!\n");
+    my_print();
     return 0;
 }
-
